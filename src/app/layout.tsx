f7f072cd9a846c037e44e80d5aaf1fe2f14b24ca
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-06-23 09:43:09 +0545

    Style layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,30 @@
-import '@/styles/globals.css'
+import { Inter } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import '@/styles/globals.css';
 
 export const metadata = {
   title: 'Breadit',
   description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+};
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang='en'
+      className={cn(
+        'bg-white text-slate-900 antialiased light',
+        inter.className
+      )}
+    >
+      <body className='min-h-screen pt-12 antialiased bg-slate-50'>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
