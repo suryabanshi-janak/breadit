c36b4d4441c560c66b3ea0a0cb0ff8a36eb992f7
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-06-25 22:29:08 +0545

    Build homepage create community section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,43 @@
+import { buttonVariants } from '@/components/ui/Button';
+import { HomeIcon } from 'lucide-react';
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <>
+      <h1 className='text-2xl font-bold md:text-3xl'>Your feed</h1>
+      <div className='grid grid-cols-1 py-6 md:grid-cols-3 gap-y-4 md:gap-x-4'>
+        {/* feed */}
+        <div></div>
+
+        {/* subreddit info */}
+        <div className='order-first overflow-hidden border border-gray-200 rounded-lg h-fit md:order-last'>
+          <div className='px-6 py-2 bg-emerald-100'>
+            <p className='font-semibold py-3 flex items-center gap-1.5'>
+              <HomeIcon className='w-4 h-4' />
+              Home
+            </p>
+          </div>
+
+          <div className='px-6 py-4 -my-3 text-sm leading-6 divide-y divide-gray-100'>
+            <div className='flex justify-between py-3 gap-x-4'>
+              <p className='text-zinc-500'>
+                Your personal Breadit homepage. Come here to check in with your
+                favorite communities.
+              </p>
+            </div>
+
+            <Link
+              href='/r/create'
+              className={buttonVariants({
+                className: 'w-full mt-4 mb-6',
+              })}
+            >
+              Create community
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
