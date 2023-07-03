e9fa3122ec1eef291cc35a315d1609fc873bc342
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-07-03 22:34:49 +0545

    Post vote api integration

--- src/components/post-vote/PostVoteClient.tsx
+++ src/components/post-vote/PostVoteClient.tsx
@@ -1,12 +1,16 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { VoteType } from '@prisma/client';
+import { PostVoteRequest } from '@/lib/validators/vote';
 import { usePrevious } from '@mantine/hooks';
-import { useCustomToast } from '@/hooks/use-custom-toast';
+import { VoteType } from '@prisma/client';
+import { useMutation } from '@tanstack/react-query';
+import axios, { AxiosError } from 'axios';
+import { useEffect, useState } from 'react';
+import { toast } from '../../hooks/use-toast';
+import { Button } from '../ui/Button';
 import { ArrowBigDown, ArrowBigUp } from 'lucide-react';
 import { cn } from '@/lib/utils';
-import { Button } from '../ui/Button';
+import { useCustomToast } from '@/hooks/use-custom-toast';
 
 interface PostVoteClientProps {
   postId: string;
@@ -20,16 +24,68 @@ const PostVoteClient = ({
   initialVote,
 }: PostVoteClientProps) => {
   const { loginToast } = useCustomToast();
-  const [votesAmt, setVotesAmt] = useState(initialVotesAmt);
+  const [votesAmt, setVotesAmt] = useState<number>(initialVotesAmt);
   const [currentVote, setCurrentVote] = useState(initialVote);
-  const prevVote = usePrevious(initialVote);
+  const prevVote = usePrevious(currentVote);
 
+  // ensure sync with server
   useEffect(() => {
     setCurrentVote(initialVote);
   }, [initialVote]);
+
+  const { mutate: vote } = useMutation({
+    mutationFn: async (type: VoteType) => {
+      const payload: PostVoteRequest = {
+        voteType: type,
+        postId: postId,
+      };
+
+      await axios.patch('/api/subreddit/post/vote', payload);
+    },
+    onError: (err, voteType) => {
+      if (voteType === 'UP') setVotesAmt((prev) => prev - 1);
+      else setVotesAmt((prev) => prev + 1);
+
+      // reset current vote
+      setCurrentVote(prevVote);
+
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 401) {
+          return loginToast();
+        }
+      }
+
+      return toast({
+        title: 'Something went wrong.',
+        description: 'Your vote was not registered. Please try again.',
+        variant: 'destructive',
+      });
+    },
+    onMutate: (type: VoteType) => {
+      if (currentVote === type) {
+        // User is voting the same way again, so remove their vote
+        setCurrentVote(undefined);
+        if (type === 'UP') setVotesAmt((prev) => prev - 1);
+        else if (type === 'DOWN') setVotesAmt((prev) => prev + 1);
+      } else {
+        // User is voting in the opposite direction, so subtract 2
+        setCurrentVote(type);
+        if (type === 'UP') setVotesAmt((prev) => prev + (currentVote ? 2 : 1));
+        else if (type === 'DOWN')
+          setVotesAmt((prev) => prev - (currentVote ? 2 : 1));
+      }
+    },
+  });
+
   return (
-    <div className='flex sm:flex-col pr-6 pb-4 sm:pb-0 gap-4 sm:gap-0 sm:w-20'>
-      <Button size='sm' variant='ghost' aria-label='upvote'>
+    <div className='flex flex-col gap-4 pb-4 pr-6 sm:gap-0 sm:w-20 sm:pb-0'>
+      {/* upvote */}
+      <Button
+        onClick={() => vote('UP')}
+        size='sm'
+        variant='ghost'
+        aria-label='upvote'
+      >
         <ArrowBigUp
           className={cn('h-5 w-5 text-zinc-700', {
             'text-emerald-500 fill-emerald-500': currentVote === 'UP',
@@ -37,11 +93,21 @@ const PostVoteClient = ({
         />
       </Button>
 
-      <p className='text-sm text-center py-2 font-medium text-zinc-900'>
+      {/* score */}
+      <p className='py-2 text-sm font-medium text-center text-zinc-900'>
         {votesAmt}
       </p>
 
-      <Button size='sm' variant='ghost' aria-label='downvote'>
+      {/* downvote */}
+      <Button
+        onClick={() => vote('DOWN')}
+        size='sm'
+        className={cn({
+          'text-emerald-500': currentVote === 'DOWN',
+        })}
+        variant='ghost'
+        aria-label='downvote'
+      >
         <ArrowBigDown
           className={cn('h-5 w-5 text-zinc-700', {
             'text-red-500 fill-red-500': currentVote === 'DOWN',
