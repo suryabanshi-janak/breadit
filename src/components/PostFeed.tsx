558c8a7e887d8858f287fad24c7b7a5e6615d99d
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-07-04 09:56:32 +0545

    Infinite scrolling setup

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { useIntersection } from '@mantine/hooks';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import axios from 'axios';
@@ -40,6 +40,12 @@ const PostFeed = ({ initialPosts, subredditName }: Props) => {
       initialData: { pages: [initialPosts], pageParams: [1] },
     }
   );
+
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+      fetchNextPage();
+    }
+  }, [entry, fetchNextPage]);
 
   const posts = data?.pages.flatMap((page) => page) ?? initialPosts;
 
